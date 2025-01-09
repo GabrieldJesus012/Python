da6eab6f665a6494f5871b02707644a6fe96b883
Gabriel de Jesus <gabrieldjesus055@outlook.com>
2025-01-09 19:02:57 -0300

    att python

---
+++ exercicios/ex003.py
@@ -0,0 +1,7 @@
+print("---------------------------------")
+print('Bem vindo ao sucesso e antecessor')
+print("---------------------------------")
+n0 = int(input('Digite o número: '))
+suc= n0 +1
+ant= n0 -1
+print('Analisando o valor {}, seu antecessor é {} e o sucessor é {}'.format(n0,ant,suc))
---
+++ exercicios/ex004.py
@@ -0,0 +1,4 @@
+n0= float(input('Primeira nota do Aluno: '))
+n1= float(input('Segunda nota do Aluno: '))
+m= (n0+n1)/2
+print('A média entre {} e {} é igual a {:.2f}'.format(n0,n1,m))
