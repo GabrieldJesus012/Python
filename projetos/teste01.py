6cc5bfce981ba49c61491521d7384c6c40d69b96
Gabriel de Jesus Silva <gabrieldjesus055@outlook.com>
2025-02-17 18:39:55 -0300

    Update teste01.py

--- projetos/teste01.py
+++ projetos/teste01.py
@@ -3,4 +3,3 @@ anonasc= int(input('Qual o ano que você nasceu?'))
 anoagor= int(input('Qual o ano de agora?'))
 idade = anoagor-anonasc
 print('Bem vindo {}! Sua idade é {} anos'.format(nome,idade))
-
