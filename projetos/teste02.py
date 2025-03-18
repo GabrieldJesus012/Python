093e58760015645d5210e21501f6ebf2578118c5
Gabriel de Jesus Silva <gabrieldjesus055@outlook.com>
2025-03-18 19:35:52 -0300

    att

--- projetos/teste02.py
+++ projetos/teste02.py
@@ -4,3 +4,4 @@ print('Prazer em te conhecer {:>20}!' .format(nome)) #20 c. alinhado a direita
 print('Prazer em te conhecer {:<20}!' .format(nome)) #20 c. alinhado a esquerda
 print('Prazer em te conhecer {:^20}!' .format(nome)) #20 c. alinhado no centro
 print('Prazer em te conhecer {:=^20}!' .format(nome)) #20 c. alinhado no centro
+print (f'Prazer em te conhecer {nome:=^20}!')
