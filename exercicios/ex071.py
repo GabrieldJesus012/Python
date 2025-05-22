b6ababa3fc9d7f37b448a957bbdfff7a4568d2c1
Gabriel de Jesus Silva <gabrieldjesus055@outlook.com>
2025-05-22 19:19:49 -0300

    disc

---
+++ exercicios/ex071.py
@@ -0,0 +1,12 @@
+alunos = {}
+alunos ['nome'] = str(input('Nome:'))
+alunos ['media'] = float(input(f'Media de {alunos["nome"]}:'))
+if alunos['media'] >=7:
+    alunos['situação'] = "Aprovado"
+elif 5<=alunos['media']<7:
+    alunos['situação'] = "Recuperação"
+else:
+    alunos['situação'] = "Reprovado"
+print('-='*20)
+for k, v in alunos.items():
+    print(f' - {k} é igual a {v}')
---
+++ projetos/teste18.py
@@ -0,0 +1,11 @@
+brasil = []
+estado1 = {'uf':'Teresina', 'sigla':'PI'}
+estado2 = {'uf':'São Paulo', 'sigla':'SP'}
+brasil.append(estado1)
+brasil.append(estado2)
+print(brasil)
+print(brasil[0])
+print(brasil[1])
+print(brasil[0]['uf'])
+
+
