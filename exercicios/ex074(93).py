d2ab5baa9977916958df542686dd5ea46e772f9a
Gabriel de Jesus Silva <gabrieldjesus055@outlook.com>
2025-05-26 20:49:55 -0300

    upt

--- exercicios/ex074(93).py
+++ exercicios/ex074(93).py
@@ -1,2 +1,9 @@
 dados = {}
+gols = []
 dados["nome"] = str(input("Nome do Jogador: "))
+partidas = int(input(f"Quantas partidas {dados['nome']} jogou? "))
+for c in range(0,partidas):
+    gols.append(int(input(f"Quantos gols na partida {c}?")))
+dados["gols"] = gols
+dados["total"] = sum(dados["gols"])
+print(dados)
