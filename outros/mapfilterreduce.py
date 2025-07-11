6b02c12b201118bd461d2ae977f13b6826101076
Gabriel de Jesus Silva <gabrieldjesus055@outlook.com>
2025-07-11 19:01:20 -0300

    filter e reduce

--- outros/mapfilterreduce.py
+++ outros/mapfilterreduce.py
@@ -1,5 +1,6 @@
 from pprint import pprint #print vem do módulo pprint (pretty-print), que significa "impressão bonita" ou "organizada".
 _print = print 
+from functools import reduce
 '''
 Quando você faz print = pprint, você substitui temporariamente a função original print pela função pprint.
 Antes disso, você salva a versão original em _print, caso queira usar depois.
@@ -13,8 +14,21 @@ produtos = [
     {'nome': 'P5', "preco": 33.22, "peso_kg":3.578,'variações':['i','j']},
 ]
 
+#map
 #precos_map = list(map(lambda p:{**p,'preco':round(p['preco']*1.05)},produtos)) #altera tanto produto e preco
-precos_map = list(map(lambda p:p['preco'],produtos))
+'''precos_map = list(map(lambda p:p['preco'],produtos))
 precos_map_listcomprehension = [produto['preco'] for produto in produtos] #fazendo a mesma coisa.
 print(precos_map)
-print(precos_map_listcomprehension)
+print(precos_map_listcomprehension)'''
+
+#filter
+'''precos_filter = list(filter(lambda p:p['preco']<10.00,produtos))
+print(precos_filter)'''
+
+#reduce
+def precoreduce(ac,p):
+    return  ac + p['preco']  # só trocar o lambda por isso caso queira
+
+preco_totais = reduce(lambda ac,p: ac+p['preco'], produtos,0)
+#preco_totais = reduce(precoreduce, produtos,0)
+print(round(preco_totais))
